d3decf2437c4c96cd7b6ba8cc82747d0abce298c
UtsavChowdhury <Utsav.Chowdhury@ril.com>
2024-09-13 12:56:14 +0530

    ECommerceAppAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularMaterialModule } from './AngularMaterialModule';
+import { AdminModule } from './admin/admin.module';
+import { CustomerModule } from './customer/customer.module';
+import { AdminComponent } from './admin/admin/admin.component';
+import { CustomerComponent } from './customer/customer/customer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterLink,RouterOutlet,AngularMaterialModule,CustomerComponent,AdminComponent,CustomerModule,AdminModule,LoginComponent,SignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
